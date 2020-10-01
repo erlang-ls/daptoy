b6dc6dbf610eebf7ec83e37dd79b3e41a6c4d6d6
Alan Zimmerman <alanzimm@fb.com>
2020-10-01 12:27:40 +0100

    Add some example launch configs for vscode

--- src/daptoy_fact.erl
+++ src/daptoy_fact.erl
@@ -1,16 +1,16 @@
 -module(daptoy_fact).
 
--export([ fact/1 ]).
+-export([ fact/1, dummy/1 ]).
 
 fact(1) ->
   1;
 fact(N) ->
   M = 1,
-  ok = dummy(),
+  ok = dummy(M),
   timer:sleep(500),
   N * fact(N - M).
 
-dummy() ->
+dummy(_A) ->
   dummy2().
 
 dummy2() ->
